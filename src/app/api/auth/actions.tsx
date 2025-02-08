6bf80dc7ec050f4f9dfd52803398ac0cda9f4f0b
AntoKeinanen <anto.keinanen@gmail.com>
2025-02-08 10:43:05 +0200

    feat: user signup and signin functionality with error handling

---
+++ src/app/api/auth/actions.tsx
@@ -0,0 +1,59 @@
+"use server";
+
+import argon2 from "argon2";
+import { db } from "~/server/db";
+import type { Error } from "~/util/error";
+import crypto from "crypto";
+import { env } from "~/env";
+
+/**
+ * Registers a new user with the provided username and password.
+ *
+ * @param {string} username - The username of the new user.
+ * @param {string} password - The password of the new user. Must be at least 8 characters long.
+ * @returns {Promise<string | Error>} A promise that resolves to the access token of the new user if successful,
+ * or an error object with a reason property if the password is too short.
+ */
+export async function signup(
+  username: string,
+  password: string,
+): Promise<string | Error> {
+  if (password.length < 8) {
+    return { reason: "PASSWORD_LENGTH" };
+  }
+
+  const passwordHash = await argon2.hash(password + env.PEPPER);
+  const accessToken = crypto.randomBytes(64).toString("hex");
+
+  await db.user.create({
+    data: {
+      username,
+      passwordHash,
+      accessToken,
+    },
+  });
+
+  return accessToken;
+}
+
+/**
+ * Signs in a user with the provided username and password.
+ *
+ * @param {string} username - The username of the user attempting to sign in.
+ * @param {string} password - The password of the user attempting to sign in.
+ * @returns {Promise<string | Error>} - A promise that resolves to the user's access token if the credentials are valid, or an error object if the credentials are invalid.
+ */
+export async function signin(
+  username: string,
+  password: string,
+): Promise<string | Error> {
+  const passwordHash = await argon2.hash(password + env.PEPPER);
+
+  const user = await db.user.findFirst({
+    where: { username, passwordHash },
+  });
+
+  if (!user) return { reason: "INV_CREDENTIALS" };
+
+  return user.accessToken as string;
+}
